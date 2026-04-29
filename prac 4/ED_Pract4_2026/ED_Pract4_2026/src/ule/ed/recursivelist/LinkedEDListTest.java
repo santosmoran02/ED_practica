d37b8643e9107242413d2250b1e66c03943f85ee
Santos <smorab02@estudiantes.unileon.es>
2026-04-29 10:33:04 +0200

    Práctica 4 de ED acabada y con VPL pasado al completo, pendiente de entregar, faltan varios días para la entrega

--- prac 4/ED_Pract4_2026/ED_Pract4_2026/src/ule/ed/recursivelist/LinkedEDListTest.java
+++ prac 4/ED_Pract4_2026/ED_Pract4_2026/src/ule/ed/recursivelist/LinkedEDListTest.java
@@ -110,5 +110,145 @@ public class LinkedEDListTest {
 		lista.getPosFirst("4");
 	}
 	
+	@Test(expected=NullPointerException.class)
+	public void test_getPosLast_null() {
+		lista.getPosLast(null);
+	}
+	
+	
+	@Test
+	public void test_getPosLast() {
+		lista.addLast("1");	
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.addLast("3");
+		lista.getPosLast("3");
+	}
+	
+	@Test(expected = NoSuchElementException.class)
+	public void test_getPosLast_noExiste() {
+	    lista.addLast("1");
+	    lista.getPosLast("7");
+	}
+	
+	@Test(expected=EmptyCollectionException.class)
+	public void test_removeLast_vacio() throws EmptyCollectionException{
+		lista.removelast();
+	}
+	
+	@Test
+	public void test_removeLast() throws EmptyCollectionException{
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.addLast("5");
+		lista.removelast();
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void test_removeLastElem_vacio() {
+		lista.removeLastElem(null);
+	}
+	
+	@Test
+	public void test_removeLastElem() {
+		lista.addLast("1");
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.addLast("3");
+		lista.removeLastElem("1");
+		lista.removeLastElem("3");
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public void test_removeLastElem_no_elemento() {
+		lista.addLast("1");
+		lista.removeLastElem("2");
+	}
+	
+	@Test
+	public void test_reverse() {
+		lista.addLast("1");
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.reverse();
+	}
+	
+	@Test
+	public void test_removeOddElements() {
+		lista.removeOddElements();
+		lista.addLast("1");
+		lista.removeOddElements();
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.removeOddElements();
+	}
+	
+	@Test
+	public void test_removeDuplicates() {
+		lista.removeConsecDuplicates();
+		lista.addLast("1");
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.addLast("4");
+		lista.removeConsecDuplicates();
+	}
+	
+	@Test(expected=EmptyCollectionException.class)
+	public void test_removePenult_vacio() throws EmptyCollectionException{
+		lista.removePenult();
+	}
+	
+	@Test(expected=NoSuchElementException.class)
+	public void test_removePenult_unElemento() throws EmptyCollectionException{
+		lista.addLast("1");
+		lista.removePenult();
+	}
+	
+	@Test
+	public void test_removePenult_dosElementos() throws EmptyCollectionException{
+		lista.addLast("1");
+		lista.addLast("2");
+		lista.removePenult();
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.addLast("5");
+		lista.removePenult();
+	}
+	
+	@Test
+	public void test_toString_vacio() {
+		lista.toString();
+	}
+	
+	@Test
+	public void test_lengthEqualsTo() {
+		lista.addLast("1");
+		lista.addLast("2");
+		lista.lengthEqualsTo(2);
+		lista.lengthEqualsTo(1);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void test_toStringExceptFromUntilReverse_invalid() {
+		lista.toSringExceptFromUntilReverse(0, 0);
+	}
+	
+	@Test
+	public void test_toStringExceptFromUntilReverse() {
+		lista.addLast("1");
+		lista.addLast("2");
+		lista.addLast("3");
+		lista.addLast("4");
+		lista.addLast("5");
+		lista.addLast("6");
+		assertEquals("(6 5 1 )", lista.toSringExceptFromUntilReverse(4, 2));
+	}
+	
 	
 }
